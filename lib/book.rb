997ec0e2fff3b9c24bab9468ecd98d511988ffa8
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-02-19 02:54:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,13 +5,14 @@ GENRES = []
   def initialize(title)
     @title = title
   end
-def genre=(genre)
-  @genre = genre
-  GENRES << genre
-end
+
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+  
+def genre=(genre)
+  @genre = genre
+  GENRES << genre
+end
 end
